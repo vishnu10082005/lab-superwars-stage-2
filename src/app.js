c9db613825e2aa3a035437fa66d4f1660bde1f41
vishnu10082005 <vishnu.preetham@kalvium.community>
2023-12-29 14:44:16 +0530

    Corrected code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-// import images from "./images/"
-// import images from "../jasmine/jasmine-3.5.0/images/super-1.png"
 const PLAYERS = [
     "Spiderman",
     "Captain America",
@@ -26,19 +24,15 @@ const PLAYERS = [
 // Initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = [];
-    for (let i = 0; i < players.length; i++) {
-        // Implement logic to create a player object with image and strength
+            // Implement logic to create a player object with image and strength
         // Your code here
-
-        // Example structure of a player object:
+    for (let i = 0; i < players.length; i++) {
         let player = {
             name: PLAYERS[i],
             strength: getRandomStrength(),
-            image: `../jasmine/jasmine-3.5.0/images/super-${i+1}.png`,
+            image: `images/super-${i + 1}.png`,
             type: i % 2 === 0 ? 'hero' : 'villain'
         };
-
-        // Push the created player object into detailedPlayers array
         detailedPlayers.push(player);
     }
     return detailedPlayers;
@@ -46,38 +40,36 @@ const initPlayers = (players) => {
 
 // Getting random strength
 const getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100); // Return a random integer between 1 and 100
+    return Math.ceil(Math.random() * 100);
 }
 
 const buildPlayers = (players, type) => {
     let fragment = '';
+// Loop through players and accumulate HTML template
+// depending on the type of player(hero|villain)
+// Your code here
 
-    // Loop through players and accumulate HTML template
-    // depending on the type of player(hero|villain)
-    // Your code here
-
-    // Example structure of HTML template fragment for a player:
+// Example structure of HTML template fragment for a player:
 
     for (let i = 0; i < players.length; i++) {
         if (players[i].type === type) {
             fragment += `<div class="player">
-                            <img src="${players[i].image}" alt="${players[i].name} ">
+                            <img src="${players[i].image}" alt="">
                             <div class="name">${players[i].name}</div>
                             <div class="strength">${players[i].strength}</div>
                         </div>`;
         }
     }
-    
 
     return fragment;
 }
 
-// Display players in HTML
+
 const viewPlayers = (players) => {
     document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
     document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
 }
-
+//Uncomment this after the code completes.
 window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
 }
